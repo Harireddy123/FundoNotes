03fafda44306ed110498039ac1cb5615a1efa770
Harireddy123 <hariprasadreddy413381@gmail.com>
2025-03-06 11:14:35 +0530

    [Hari]Added : ForgetPasswordModel,ResetPasswordModel,send

---
+++ FundoNotes.Data/Models/ForgetPasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class ForgetPasswordModel
+    {
+        [Required(ErrorMessage = "User ID is required")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Token is required")]
+        public string Token { get; set; }
+    }
+}
---
+++ FundoNotes.Data/Models/Send.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class Send
+    {
+        public string SendMail(string ToEmail, string Token)
+        {
+            string FromEmail = "hariprasadreddy413381@gmail.com";
+            MailMessage message = new MailMessage(FromEmail, ToEmail);
+            string MailBody = "The token for the reset password: " + Token;
+            message.Subject = "Token generated for resetting password";
+            message.Body = MailBody.ToString();
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential credential = new NetworkCredential("hariprasadreddy413381@gmail.com", "ilhc axdk dxgq njhh");
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = credential;
+            smtpClient.Send(message);
+
+            return ToEmail;
+        }
+    }
+}
